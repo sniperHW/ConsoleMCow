70b013916dbe1e68ff245be8edaa720cd62a5299
智勇 徐 <xuzy1975@hotmail.com>
2022-09-06 15:17:46 +0800

    9/6 by xzy

--- CActionLine.h
+++ CActionLine.h
@@ -10,13 +10,13 @@ class CActionLine
 
 private:
 	std::string m_sReserve; //当前街已经解析的
-	std::string m_sPreflopType; 
+	std::string m_sNodeType; 
 
 	bool MatchPreflopNode(const std::string& sAbbrLine, std::string& sNodeName);
 
 public:
-	bool Parse(const std::string& sActionLine, ActionLineMode msgMode, std::string & sNodeName, CGame & game); //offline或多人返回false,更新game相关，取得NodeName, 记录raisesize，记录preflop类型，sActionLine为增量
-	const std::string & GetPreflopType();
+	bool Parse(const std::string& sActionLine, const ActionLineMode msgMode, std::string & sActionSquence, CGame & game, StackByStrategy& curStackByStrategy); //offline或多人返回false,更新game相关，取得NodeName, 记录raisesize，记录preflop类型，sActionLine为增量
+	const std::string & GetNodeType();
 	void ChangeRound();
 	
 };
--- CRange.h
+++ CRange.h
@@ -17,15 +17,18 @@ public:
 	CRange(RelativePosition heroRelativePosition);
 
 	bool Load(GameType gmType, const std::string & sNodeName, const CBoard & boardNext); //file模式，同时排除公牌
-	bool Load(GameType gmType, const std::string & sNodeName, const CBoard & boardNext, bool blIsWizard); //wizard模式，同构转换，排除公牌，忽略blIsWizard
-	bool Load(GameType gmType, const Json::Value & root, const std::string & sActionSquence, const CBoard & boardNext); //solver模式，同构转换，排除公牌
+	bool Load(GameType gmType, const std::string & sActionSquence, const StackByStrategy& stack, const CBoard & boardNext, bool blIsWizard); //wizard模式，同构转换，排除公牌，忽略blIsWizard
+	bool Load(GameType gmType, const Json::Value & root, const std::string & sActionSquence, const StackByStrategy& stack, const CBoard & boardNext); //solver模式，同构转换，排除公牌
 
 	std::string GetRangeStr(); //转为solver配置文件需要的格式
 	void Clear();
 
 private:
-	void ConvertIsomorphism(const SuitReplace & suitReplace); //转换同构
+	void ConvertIsomorphism(const SuitReplace & suitReplace); //转换同构=
 	void RemoveComboByBoard(const CBoard & pBoard); //排除公牌相关的组合
+
+	int MatchBetSize(double dActuallySize, const std::vector<double>& candidateSizes, GameType gmType, const StackByStrategy& stack);	//按实际下注bb数，匹配子节下注空间，用于sover解计算，参数都为实际size，返回为匹配的siz
+	int MatchBetRatio(double dActuallyRatio, const std::vector<double>& candidateRatios, GameType gmType, const StackByStrategy& stack);	//按实际下注比例，匹配子节下注空间，用于wizard解计算，需要先将size转为比例，候选在策略树设置中，返回为匹配的比例
 };
 
 #endif
