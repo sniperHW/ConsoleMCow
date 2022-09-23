58a3c6d9f32653b5d50d78effae14a10ef9fef6f
sniperHW <sniperHW@163.com>
2022-09-23 09:18:07 +0800

    Combo:

--- CCombo.cpp
+++ CCombo.cpp
@@ -4,15 +4,61 @@
 using namespace std;
 
 
+//0:红色,1:黑色
+static int getColor(char c){
+	if(c == 'd' || c =='h') {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
 vector<string> CCombo::GetCombosByAbbr(const string& sAbbr)
 {
-	return vector<string>{};
+	vector<string> combos;
+	
+	if(sAbbr == "AA"){
+		for(auto it = ComboMapping.begin();it != ComboMapping.end();it++) {
+			if((*it)[0] == (*it)[1]) {
+				combos.push_back(*it);
+			}			
+		}
+	}
+
+
+
+	if(sAbbr == "AKo" || sAbbr == "AK") {
+		for(auto it = ComboMapping.begin();it != ComboMapping.end();it++) {
+			if(getColor((*it)[1]) != getColor((*it)[3])){
+				combos.push_back(*it);
+			}
+		}
+	}
+
+
+	if(sAbbr == "AKs" || sAbbr == "AK") {
+		for(auto it = ComboMapping.begin();it != ComboMapping.end();it++) {
+			if(getColor((*it)[1]) == getColor((*it)[3])){
+				combos.push_back(*it);
+			}
+		}
+	}
+
+	return combos;
 }
+
 
 string CCombo::GetAbbrSymble(const string sCombo)
 {
+	if(sCombo[0] == sCombo[2]) {
+		return "AA";
+	}
 
-	return string{};
+	if(getColor(sCombo[1]) == getColor(sCombo[3])) {
+		return "AKs";
+	}else{
+		return "AKo";
+	}	
 }
 
 void CCombo::SetHands(const string sCombo)
